746ed6afdf747c9f9c7e71fe0dfb2f809282c5a6
Kathambari-Kumar <sanika1250@gmail.com>
2024-11-11 12:58:35 +0100

    added create movie service

--- Services/MovieServices.cs
+++ Services/MovieServices.cs
@@ -1,11 +1,31 @@
-﻿using MovieShop.Models.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieShop.Data;
+using MovieShop.Models.Db;
 
 namespace MovieShop.Services
 {
     public class MovieServices : IMovieServices
     {
-        public void Create(Movie movie) 
+        private readonly interstellardb _db;
+
+        public MovieServices(interstellardb db) 
         { 
+            _db = db;
+        }
+        public void Create(Movie movie) 
+        {
+            try
+            {
+                if (movie != null)
+                {
+                    _db.Movies.Add(movie);
+                    _db.SaveChanges();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Field Values should not be null!");
+            }
         }
         public List<Movie> Display()
         {
