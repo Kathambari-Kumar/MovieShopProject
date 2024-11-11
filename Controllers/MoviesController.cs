86e18bb11c8aa15b73e2d8090246bae81ddec378
MansiHosseini <Mansi.hosseini@gmail.com>
2024-11-11 10:49:44 +0100

    nothing added conflict resolved

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,16 +5,8 @@ namespace MovieShop.Controllers
     public class MoviesController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Display()
-
-        {
-            return View();
-        }
-
+       
 
     }
 }
