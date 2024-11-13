fc16d18e35a53f800e2f2f0b7b948d28abf13251
EdenPau <achiengpauline2@gmail.com>
2024-11-13 11:21:53 +0100

    Edited the addcart action

--- Extensions/SessionExtention.cs
+++ Extensions/SessionExtention.cs
@@ -4,7 +4,7 @@ namespace MovieShop.Extensions
 {
     public static class SessionExtention
     {
-        public static void set<T>(this ISession session,string key,T value)
+        public static void Set<T>(this ISession session,string key,T value)
         {
             session.SetString(key,JsonSerializer.Serialize(value));
         }
