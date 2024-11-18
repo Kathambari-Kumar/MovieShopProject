e8a059c40a45e04a6d90748a0ab649926874dc0e
EdenPau <achiengpauline2@gmail.com>
2024-11-18 21:46:03 +0100

    added functionality for customer orders

---
+++ Models/ViewModels/CustomerOrder.cs
@@ -0,0 +1,27 @@
+﻿using MovieShop.Models.Db;
+
+namespace MovieShop.Models.ViewModels
+{
+    public class CustomerOrder
+    {
+        public string Firstname { get; set; } = string.Empty;
+
+        public string Lastname { get; set; } = string.Empty ;
+
+        public string CustomerName =>$"{Firstname} {Lastname}";
+
+        public string MovieTitle { get; set; } = string.Empty;
+
+        public string DateOfPurchase { get; set; } = string.Empty;
+
+        public double Price { get; set; } //price of individual movie
+
+       
+
+        public int TotalOrderCount { get; set; }
+
+       
+
+       
+    }
+}
---
+++ Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using MovieShop.Models.Db;
+using MovieShop.Models.ViewModels;
+
+namespace MovieShop.Services
+{
+    public interface IOrderService
+    {
+
+        public List<CustomerOrder> GetCustomerOrders(string email);
+    }
+}
