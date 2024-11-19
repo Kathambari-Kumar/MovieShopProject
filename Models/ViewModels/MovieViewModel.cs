9cf4958521c7d589e6615803843ceb85239afbc0
EdenPau <achiengpauline2@gmail.com>
2024-11-19 11:52:18 +0100

    customer orders

---
+++ Models/ViewModels/MovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieShop.Models.ViewModels
+{
+    public class MovieViewModel
+    {
+
+        public string Title { get; set; }= string.Empty;
+
+
+        public double Price { get; set; }   
+    }
+}
